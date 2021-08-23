e549627f279d3f8e654ff40973b6d44e72beb1d8
bobdubei <vovandv@gmail.com>
2021-08-23 18:43:20 +0300

    View controller

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var helloBobLabel: UILabel!
+    @IBOutlet var showTextButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func showTextButtonPressed() {
+    }
+    
+    
 }
 
